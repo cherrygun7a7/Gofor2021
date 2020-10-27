f7a0d653349c5024d9e692e7ba742eb789fc22d5
cherrygun7a7 <mcc_lie@icloud.com>
2020-10-26 22:06:07 -0400

    Component3.0--Add new problems

--- week1/oj-client/src/app/services/data.service.ts
+++ week1/oj-client/src/app/services/data.service.ts
@@ -1,20 +1,28 @@
 import { Injectable } from '@angular/core';
 import { Problem } from '../models/problem.model';
 import { PROBLEMS } from "../mock-problems";
+
 @Injectable({
   providedIn: 'root'
 })
 export class DataService {
 
+  problems: Problem[] = PROBLEMS;
+
   constructor() { }
 
   //get all problems
   getProblems(): Problem[] {
-    return PROBLEMS;
+    return this.problems;
   }
 
   //get problem by id
   getProblem(id: number): Problem {
-    return PROBLEMS.find((problem) => problem.id === id);
+    return this.problems.find((problem) => problem.id === id);
+  }
+
+  addProblem(problem: Problem): void{
+    problem.id = this.problems.length  + 1;
+    this.problems.push(problem);
   }
 }
